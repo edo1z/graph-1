58dd3a9ff43ff01bc1b603d696c2d4739caa0b07
web3ten0 <web3ten0@gmail.com>
2022-01-29 22:15:29 +0900

    code format

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ impl Graph {
         for pair in pairs.iter() {
             vertices[pair.0 - 1].push(pair.1 - 1);
             vertices[pair.1 - 1].push(pair.0 - 1);
-        } 
-        Graph{vertices}
+        }
+        Graph { vertices }
     }
 
     fn dfs(&self) -> bool {
@@ -17,43 +17,28 @@ impl Graph {
         self._dfs(&mut visited, 0);
         visited.iter().find(|&&x| !x) == None
     }
-    fn _dfs(&self, visited: &mut Vec<bool>, idx:usize) {
+    fn _dfs(&self, visited: &mut Vec<bool>, idx: usize) {
         visited[idx] = true;
         for i in self.vertices[idx].iter() {
-            if !visited[*i] {self._dfs(visited, i.clone())}
+            if !visited[*i] {
+                self._dfs(visited, i.clone())
+            }
         }
     }
 }
 
 fn main() {
     let vertices_num = 6;
-    let pairs = vec![
-        (1,2), 
-        (1,3),
-        (2,4),
-        (2,5),
-        (3,4),
-        (4,6),
-        (5,6),
-    ];
+    let pairs = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3, 4), (4, 6), (5, 6)];
     let graph = Graph::new(vertices_num, &pairs);
-    println!("{}", if graph.dfs() {"Yes"} else {"No"});
+    println!("{}", if graph.dfs() { "Yes" } else { "No" });
 }
 
 #[test]
 fn test_graph() {
-    let pairs = vec![
-        (1,2), 
-        (1,3),
-        (2,4),
-        (2,5),
-        (3,4),
-        (4,6),
-        (5,6),
-    ];
+    let pairs = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3, 4), (4, 6), (5, 6)];
     let graph = Graph::new(6, &pairs);
     assert_eq!(graph.dfs(), true);
     let graph = Graph::new(7, &pairs);
     assert_eq!(graph.dfs(), false);
 }
-
