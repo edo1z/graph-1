f701181e3d9a18725072752f05273e2178ba7307
web3ten0 <web3ten0@gmail.com>
2022-01-31 23:38:50 +0900

    added best_route_map

--- src/maze/bfs.rs
+++ src/maze/bfs.rs
@@ -1,14 +1,14 @@
 use crate::maze::dist::Dist;
 use crate::maze::posi::Posi;
 use crate::maze::prev::Prev;
-use crate::maze::{Maze, MazeVec};
+use crate::maze::Maze;
 use std::collections::VecDeque;
 
 pub trait Bfs {
     fn bfs(&self) -> (Dist, Prev);
-    fn bfs_route(&self, prev: &Prev) -> VecDeque<Posi>;
     fn bfs_dist_map(&self, dist:&Dist) -> String;
-    fn bfs_best_route_map(&self, routes:VecDeque<Posi>) -> String;
+    fn bfs_best_route(&self, prev: &Prev) -> VecDeque<Posi>;
+    fn bfs_best_route_map(&self, routes:&VecDeque<Posi>) -> String;
 }
 impl Bfs for Maze {
     fn bfs(&self) -> (Dist, Prev) {
@@ -48,7 +48,7 @@ impl Bfs for Maze {
         dist_map
     }
 
-    fn bfs_route(&self, prev: &Prev) -> VecDeque<Posi> {
+    fn bfs_best_route(&self, prev: &Prev) -> VecDeque<Posi> {
         let mut route: VecDeque<Posi> = VecDeque::from([self.goal_posi.clone()]);
         let mut current_posi = &self.goal_posi;
         loop {
@@ -62,8 +62,23 @@ impl Bfs for Maze {
         route
     }
 
-    fn bfs_best_route_map(&self, routes:VecDeque<Posi>) -> String {
-        String::from("hoge")
+    fn bfs_best_route_map(&self, routes:&VecDeque<Posi>) -> String {
+        let mut route_map_vec = vec![vec![' '; self.width as usize]; self.height as usize];
+        let mut route_map = String::from("");
+        for posi in routes.iter() {
+            route_map_vec[posi.row as usize][posi.col as usize] = '★';
+        }
+        for (row, row_val) in self.maze_vec.iter().enumerate() {
+            for (col, &c) in row_val.iter().enumerate() {
+                if c == self.start_char || c == self.goal_char || c == self.wall_char {
+                    route_map.push_str(&format!("{} | ", c));
+                } else {
+                    route_map.push_str(&format!("{} | ", route_map_vec[row][col]));
+                }
+            }
+            route_map.push_str("\n");
+        }
+        route_map
     }
 }
 
@@ -90,5 +105,25 @@ mod tests {
 "#;
         assert_eq!(dist_map, ans.to_string());
 
+    }
+
+    #[test]
+    fn test_maze_bfs_best_route_map() {
+        let maze_str = maze_str::maze_str1();
+        let maze_vec = Maze::maze_vec_from_string(maze_str);
+        let maze = Maze::new(maze_vec, 'S', 'G', '#');
+        let (_dist, prev) = maze.bfs();
+        let route = maze.bfs_best_route(&prev);
+        let route_map = maze.bfs_best_route_map(&route);
+        let ans = r#"  | # | ★ | ★ | ★ |   | # | G | 
+  | # | ★ | # | ★ | ★ | ★ | ★ | 
+  | ★ | ★ | # |   | # | # |   | 
+# | ★ | # | # |   |   |   | # | 
+★ | ★ |   | # | # | # |   | # | 
+★ | # |   |   |   |   |   | # | 
+★ |   |   | # |   | # |   |   | 
+S |   |   |   |   |   |   |   | 
+"#;
+        assert_eq!(route_map, ans.to_string());
     }
 }
